709746cf24bb6583cd4ae51e7eef4765a4d206ca
h4lllllll <hjod1172@yahoo.co.jp>
2023-01-14 11:40:44 +0000

    fix

--- backend/src/global.ts
+++ backend/src/global.ts
@@ -2,18 +2,18 @@
  * type
  */
 export type FormData = {
-  name: string
-  email: string
-}
+  name: string;
+  email: string;
+};
 
 declare global {
-  function getData(): GoogleAppsScript.Content.TextOutput
-  function getData2(): JSON
-  function echo(msg: string): string
-  function echo2(s1: string, s2: string): JSON
-  function doGet(): GoogleAppsScript.HTML.HtmlOutput
-  function setData(data: FormData): JSON
+  function getData(): GoogleAppsScript.Content.TextOutput;
+  function getData2(): JSON;
+  function echo(msg: string): string;
+  function echo2(s1: string, s2: string): JSON;
+  function doGet(): GoogleAppsScript.HTML.HtmlOutput;
+  function setData(data: FormData): JSON;
 
   /** globalとかglobalthisとかわけわからん */
 }
-export default global
+export default global;
